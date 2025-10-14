0bbc80881bd584d244d9bb0ddd2aafe9d4b166f2
VISHNU23CSB61 <153609450+VISHNU23CSB61@users.noreply.github.com>
2025-10-14 22:56:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function handleAICapture() {
+    alert("AI Scanning your inbox for new bookings. This process is instant and secure.");
+}
+
+function viewItinerary() {
+    alert("Itinerary is secured. Full routing details loaded.");
+}
