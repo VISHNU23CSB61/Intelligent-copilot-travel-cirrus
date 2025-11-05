0c1002365b36f65ac2ca2edba0b83199f431829d
VISHNU23CSB61 <153609450+VISHNU23CSB61@users.noreply.github.com>
2025-11-05 20:34:03 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function handleAICapture() {
-    alert("AI Scanning your inbox for new bookings. This process is instant and secure.");
-}
-
-function viewItinerary() {
-    alert("Itinerary is secured. Full routing details loaded.");
-}
